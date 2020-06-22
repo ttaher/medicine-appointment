18267cea70bb0114f09abfc5c8bd730a8962fbfe
ttaher <talat.abdelbaky@gmail.com>
2020-06-22 17:45:18 +0200

    first commit

---
+++ src/components/AddAppointments.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AddAppointments extends Component {
+    render() {
+        return <div>Add test  Appointments</div>
+    }
+}
+
+export default AddAppointments;
---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+      lastIndex: 1
+    }
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        })
+
+        this.setState({
+          myAppointments: apts
+        })
+      });
+
+  }
+  render() {
+    return (
+      <main className="page bg-white" id="petratings" >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {console.log(this.state.myAppointments)}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
